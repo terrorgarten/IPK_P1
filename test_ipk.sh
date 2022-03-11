5ea6bf17d97fc8cf7683dc447e4058cbc6f1f975
Matěj <xkonop03@vutbr.cz>
2022-03-11 11:18:51 +0100

    Fixed test script for github actions (tput err)

--- test_ipk.sh
+++ test_ipk.sh
@@ -2,32 +2,32 @@
 echo "$(tput setaf 1)MAKING"
 make
 
-echo $"$(tput setaf 1) launching on port 1500 $(tput sgr0)"
+echo "Launching on port 1500"
 ./hinfosvc 1500 &>server_output.log &
 
-echo -e "$(tput setaf 1) Getting hostname:$(tput sgr0)"
+echo -e "Getting hostname:"
 GET http://localhost:1500/hostname
 
-echo -e "$(tput setaf 1) Getting load:$(tput sgr0)"
+echo -e "Getting load:"
 GET http://localhost:1500/load
 
-echo -e "\n$(tput setaf 1) Getting CPU name:$(tput sgr0)"
+echo -e "\nGetting CPU name:"
 GET http://localhost:1500/cpu-name
 
-echo "$(tput setaf 1) Killing the server$(tput sgr0)"
+echo "Killing the server"
 pkill -f "hinfosvc"
 
-echo "$(tput setaf 1) relaunching$(tput sgr0)"
+echo "relaunching"
 ./hinfosvc 1500 &>server_output.log &
 
-echo -e "$(tput setaf 1) Getting hostname (2):$(tput sgr0)"
+echo -e "Getting hostname (2):"
 GET http://localhost:1500/hostname
 
-echo -e "$(tput setaf 1) Getting load (2):$(tput sgr0)"
+echo -e "Getting load (2):"
 GET http://localhost:1500/load
 
-echo -e "\n$(tput setaf 1) Getting CPU name (2):$(tput sgr0)"
+echo -e "Getting CPU name (2):"
 GET http://localhost:1500/cpu-name
 
-echo "$(tput setaf 1) Killing the server (2)$(tput sgr0)"
+echo "Killing the server.."
 
