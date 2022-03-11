0366e47eea7dea7067cdc191dcd1f685429db8fd
Matěj <xkonop03@vutbr.cz>
2022-03-11 11:30:39 +0100

    added curl tests

--- test_ipk.sh
+++ test_ipk.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-echo "MAKING"
+echo "---------MAKING-----------"
 make
+
+echo "-------GET TESTS--------"
 
 echo "Launching on port 1500"
 ./hinfosvc 1500 &>server_output.log &
@@ -17,7 +19,7 @@ GET http://localhost:1500/cpu-name
 echo "Killing the server"
 pkill -f "hinfosvc"
 
-echo "relaunching"
+echo "Relaunching"
 ./hinfosvc 1500 &>server_output.log &
 
 echo -e "Getting hostname (2):"
@@ -29,5 +31,25 @@ GET http://localhost:1500/load
 echo -e "Getting CPU name (2):"
 GET http://localhost:1500/cpu-name
 
-echo "Killing the server.."
+echo "-------CURL TESTS--------"
+touch curl_cpu-name
+touch curl_hostname
+touch curl_load
 
+echo "HOSTNAME:"
+curl  http://localhost:1500/hostname --verbose --output curl_hostname
+cat curl_hostname
+
+echo "LOAD:"
+curl  http://localhost:1500/load --verbose --output curl_load
+cat curl_load
+echo "HOSTNAME:"
+
+curl  http://localhost:1500/cpu-name --verbose --output curl_cpu-name
+cat curl_cpu-name
+
+rm curl_cpu-name
+rm curl_hostname
+rm curl_load
+
+echo "END"
